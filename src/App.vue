a4594516b70bb9508dd3090dce26e3694a79b593
zhangzhaoxiang <1448691084@qq.com>
2019-04-10 22:48:06 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      title:"这是我的第一个vue脚手架项目"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
